60d6baf3cefca7ce64c5a84333efde36e3a3ba0e
Sergio <sergioottati@hotmail.com>
2024-10-18 16:34:16 -0300

    Menu drawer terminado

--- app/src/main/java/com/example/primertplogin/UserActivity.kt
+++ app/src/main/java/com/example/primertplogin/UserActivity.kt
@@ -2,29 +2,76 @@ package com.example.primertplogin
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.primertplogin.RegisterActivity.Companion.CREDENTIALS
 import com.example.primertplogin.databinding.ActivityUserBinding
+import com.google.android.material.navigation.NavigationView
 
-class UserActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
   private lateinit var binding: ActivityUserBinding
+
+  private lateinit var drawerLayout : DrawerLayout
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityUserBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
+    val toolbar = binding.toolbar
+    setSupportActionBar(toolbar)
+
+    drawerLayout = binding.main
+
+    val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_home_open, R.string.nav_drawer_home_close)
+    drawerLayout.addDrawerListener(toggle)
+
     val preferences = getSharedPreferences(CREDENTIALS, MODE_PRIVATE)
 
-    binding.button.setOnClickListener {
-      preferences.edit().remove("autoLogin").apply()
-      goToLogin()
-      }
+
+
+//    binding.button.setOnClickListener {
+  //    preferences.edit().remove("autoLogin").apply()
+    //  goToLogin()
+      //}
+
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    supportActionBar?.setHomeButtonEnabled(true)
+    supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+
+    binding.navigationView.setNavigationItemSelectedListener(this, )
     }
 
   private fun goToLogin() {
     val intent = Intent(this, MainActivity::class.java)
     startActivity(intent)
     }
+
+  override fun onNavigationItemSelected(item: MenuItem): Boolean {
+    when(item.itemId) {
+      R.id.nav_item_drama -> {
+        Toast.makeText(this, "Drama", Toast.LENGTH_SHORT).show()
+      }
+      R.id.nav_item_accion -> {
+        Toast.makeText(this, "Acción", Toast.LENGTH_SHORT).show()
+      }
+      R.id.nav_item_susp -> {
+        Toast.makeText(this, "Suspenso", Toast.LENGTH_SHORT).show()
+      }
+      R.id.nav_item_fant -> {
+        Toast.makeText(this, "Fantasia", Toast.LENGTH_SHORT).show()
+      }
+      R.id.nav_item_comedia -> {
+        Toast.makeText(this, "Comedia", Toast.LENGTH_SHORT).show()
+      }
+    }
+
+    drawerLayout.closeDrawer(GravityCompat.START)
+    return true
   }
+}
